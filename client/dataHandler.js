3a6c856647d0a46fc8c005d468efde519c813f39
Song-Li <lisong1013@gmail.com>
2018-10-09 19:50:02 +0000

    update row code of client

---
+++ client/dataHandler.js
@@ -0,0 +1,71 @@
+class DataHandler = function() {
+  /**
+   * A handler class for receving and sending data to Server
+   *
+   * @access    public
+   * @constructs DataHandler(), DataHandler(server_address)
+   *
+   * @param server_address attributes   the server address that will interact with 
+   *
+   */
+  constructor(server_address) {
+    this.server_address = server_address;
+  }
+
+  constructor() {
+    this.server_address = "";
+    this.unique_label = this.request_unique_label();
+    this.recordID = "";
+  }
+
+  var _this = this;
+  this.request_unique_label = function() {
+
+  }
+
+  var doSend = function (app_name, key_value_pairs, async = false, encoded = true) {
+    /**
+     * do send to the server.
+     *
+     * This function will send the key_value pairs with encoded URI to prevent the bugs
+     * introduced by special chars
+     *
+     * @param app_name string   the aim app name to handle this send event
+     * @param key_value_pairs map   the key value pairs. Example: {key = value}
+     * @param async boolean     async or not. If false, return when finished
+     *
+     * @return result string    the handle result returned from the server
+     */
+
+    var xhttp = new XMLHttpRequest();
+    var url = `${this.server_address}/${app_name}`;
+    var data = JSON.stringify(key_value_pairs);
+
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var res = this.responseText;
+        return res
+      }
+    };
+
+    xhttp.open("POST", url, async);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    if(encoded){
+      xhttp.send(encodeURI(data));
+    } else {
+      xhttp.send(data);
+    }
+  }
+
+  this.storePicture = function(dataURL) {
+    var data = "imageBase64=" + encodeURIComponent(dataURL); 
+    return this.doSend('pictures', data, async = true)
+  }
+
+  this.updateFeatures = function(feature_data){
+    feature_data['unique_label'] = this.unique_label;
+    app_name = 'updateFeatures';
+    var data_str = JSON.stringify(feature_data) 
+    this.doSend(app_name, data_str)
+  }
+}
